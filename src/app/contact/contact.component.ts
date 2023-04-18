09b9d469d07b7e5f96c7bfc3fdc2cf01dd91168e
Natalia Ortiz <natorgom@gmail.com>
2023-04-18 23:30:16 +0200

    add delete contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,24 +5,22 @@ import { Contact, ContactService } from './contact.service';
 @Component({
   selector: 'app-contact-component',
   template: ` <app-bar></app-bar>
+
     <div class="contacts-container">
-      <mat-accordion>
+      <div class="contacts-actions">
+        <div class="action">Add contact</div>
+      </div>
+      <mat-accordion class="contacts-list">
         <mat-expansion-panel *ngFor="let contact of contacts$ | async">
           <mat-expansion-panel-header>
             <mat-panel-title>
               {{ contact.surname }}, {{ contact.name }}
             </mat-panel-title>
           </mat-expansion-panel-header>
-          <div>
-            <span class="details-title">Phone number:</span>
-            {{ contact.phoneNumber }}
-          </div>
-          <div>
-            <span class="details-title">Email:</span> {{ contact.email }}
-          </div>
-          <div>
-            <span class="details-title">Address:</span> {{ contact.address }}
-          </div>
+          <app-contact-details-component
+            [contact]="contact"
+            (deleteContact)="onDeleteContact(contact.id)"
+          ></app-contact-details-component>
         </mat-expansion-panel>
         <div></div>
       </mat-accordion>
@@ -43,5 +41,9 @@ export class ContactComponent implements OnInit {
         tap((list) => this.contacts$.next(list))
       )
       .subscribe();
+  }
+
+  onDeleteContact(id: string) {
+    this.contactService.deleteContact(id);
   }
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,6 +1,7 @@
 import { AppBarModule } from '../app-bar';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
+import { ContactDetailsComponent } from './contact-details.component';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactService } from './contact.service';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,7 +16,7 @@ import { NgModule } from '@angular/core';
     MatButtonModule,
     MatExpansionModule,
   ],
-  declarations: [ContactComponent],
+  declarations: [ContactComponent, ContactDetailsComponent],
   providers: [ContactService],
   exports: [ContactComponent],
 })
