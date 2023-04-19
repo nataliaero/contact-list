59c47a612c971a5a1e5d45c8098b72a77f93b3f0
Natalia Ortiz <natorgom@gmail.com>
2023-04-19 10:09:35 +0200

    add form for new contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,13 +2,16 @@ import { BehaviorSubject, take, tap } from 'rxjs';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Contact, ContactService } from './contact.service';
 
+import { ContactDialogComponent } from './contact-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-contact-component',
   template: ` <app-bar></app-bar>
 
     <div class="contacts-container">
       <div class="contacts-actions">
-        <div class="action">Add contact</div>
+        <div class="action" (click)="openAddContactDialog()">Add contact</div>
       </div>
       <mat-accordion class="contacts-list">
         <mat-expansion-panel *ngFor="let contact of contacts$ | async">
@@ -29,7 +32,10 @@ import { Contact, ContactService } from './contact.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactComponent implements OnInit {
-  constructor(private contactService: ContactService) {}
+  constructor(
+    private contactService: ContactService,
+    public dialog: MatDialog
+  ) {}
 
   contacts$ = new BehaviorSubject<Contact[]>([]);
 
@@ -45,5 +51,9 @@ export class ContactComponent implements OnInit {
 
   onDeleteContact(id: string) {
     this.contactService.deleteContact(id);
+  }
+
+  openAddContactDialog() {
+    this.dialog.open(ContactDialogComponent);
   }
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -2,11 +2,16 @@ import { AppBarModule } from '../app-bar';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
 import { ContactDetailsComponent } from './contact-details.component';
+import { ContactDialogComponent } from './contact-dialog.component';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactService } from './contact.service';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -14,9 +19,17 @@ import { NgModule } from '@angular/core';
     CommonModule,
     ContactRoutingModule,
     MatButtonModule,
+    MatDialogModule,
     MatExpansionModule,
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
-  declarations: [ContactComponent, ContactDetailsComponent],
+  declarations: [
+    ContactComponent,
+    ContactDialogComponent,
+    ContactDetailsComponent,
+  ],
   providers: [ContactService],
   exports: [ContactComponent],
 })
